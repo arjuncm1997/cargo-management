a7820bbcf5a85b1ac2b23a1b585e8942d663808d
arjuncm1997 <arjun>
2020-02-28 17:07:49 +0530

    no

--- cargo/models.py
+++ cargo/models.py
@@ -1,1 +1,28 @@
- 
+from cargo import db, app, login_manager
+from flask_login import UserMixin
+from flask_table import Table, Col, LinkCol
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(db.Model, UserMixin):
+
+    id=db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80),nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+@login_manager.user_loader
+def loaduser(id):
+    return Ship.query.get(int(id))
+
+class Ship(db.Model, UserMixin):
+
+    id=db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80),nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
--- cargo/routes.py
+++ cargo/routes.py
@@ -1,9 +1,10 @@
 import os 
 from flask import Flask, flash, session
 from flask import render_template, flash, redirect, request, abort, url_for
-from cargo import app
-
-
+from cargo import app, db, bcrypt
+from cargo.models import User, Ship
+from flask_login import login_user, current_user, logout_user, login_required
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route('/')
@@ -34,15 +35,63 @@ def uindex():
 def sindex():
     return render_template("sindex.html")
 
-@app.route('/userregister')
-def userregister():
-    return render_template("userregister.html")
 
-@app.route('/login')
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    if request.method == 'POST':
+        user = User.query.filter_by(email=request.form['email'] ).first()
+        user1 = Ship.query.filter_by(email=request.form['email']).first()
+        if user and bcrypt.check_password_hash(user.password,request.form['password']):
+            login_user(user)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect('/uindex')
+        if user1 and bcrypt.check_password_hash(user1.password, request.form['password']):
+            login_user(user1)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect('/sindex')
+
+        else:
+            flash('Login Unsuccessful. Please check email and password', 'danger')
+    return render_template('login.html', title='Login')
+
+@app.route('/userregister',methods=['GET','POST'])
+def userregister():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        hashed_password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        new = User(username = name,email=email,password=hashed_password )
+
+        try:
+            db.session.add(new)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        return render_template('userregister.html')
 
 
-@app.route('/shipregister')
+@app.route('/shipregister',methods=['GET','POST'])
 def shipregister():
-    return render_template("shipregister.html")
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        hashed_password = bcrypt.generate_password_hash(request.form['password']).decode('utf-8')
+        new = Ship(username = name,email=email,password=hashed_password )
+
+        try:
+            db.session.add(new)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        return render_template('shipregister.html')
+
+
+@app.route('/sdetails')
+def sdetails():
+    return render_template("sdetails.html")
